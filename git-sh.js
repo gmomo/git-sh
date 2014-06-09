5e9783fd680722ce3b066c0cc5192e9787699060
Sinuhe MB <gnu.ico@gmail.com>
2014-06-09 18:14:38 -0500

    Update git-sh.js
    Agregar función exec sync

--- git-sh.js
+++ git-sh.js
@@ -2,6 +2,7 @@
 
 var readline = require('readline');
 var exec = require('child_process').exec;
+var child_process = require('child_process');
 var fs = require('fs');
 
 var CMD_SH;
@@ -9,7 +10,7 @@ var KEYS;
 var PROMPT='git-sh> ';
 var FILE = __dirname + '/cmd.json';
 var UTF8 = 'utf8';
- 	 	
+
 var rl;
 
 /*
@@ -17,7 +18,6 @@ var rl;
  */
 
 main();
-
 
 /*
  * FUNCTION
@@ -42,12 +42,22 @@ function main(){
 	rl.prompt();
 
 	rl.on('line', function(line) {
-		var cmd = line.split(' ')[0];
-		var arg = line.substring(cmd.length,line.length)
 
-		if(CMD_SH[cmd] != undefined){
-			run_cmd(CMD_SH[cmd],arg);
+		if(line == 'man'){
+			console.log('\nAlias \t Comando');
+			console.log('===================');
+			for(var k in CMD_SH){		
+				console.log(k);
+				console.log('\t'+CMD_SH[k]+'\n');
+			}		
+		}else{
+			var cmd = line.split(' ')[0];
+			var arg = line.substring(cmd.length,line.length)
+			if(CMD_SH[cmd] != undefined){
+				run_cmd(CMD_SH[cmd],arg);
+			}
 		}
+
 		rl.prompt();
 
 	}).on('SIGINT', function() {
@@ -70,12 +80,45 @@ function completer(line) {
 function run_cmd(cmd,arg){
 	
 	var child = exec(cmd+arg,
-			function (error, stdout, stderr) {
-				rl.setPrompt('');
-				rl.write('\n');				
-				console.log(stdout);
-				rl.setPrompt(PROMPT);
-				rl.write('\n');
-			});
+			function (error, stdout, stderr) {		
+				var msg;
 	
+				if(stdout != ''){
+					msg=stdout;
+				}else if (stderr != ''){
+					msg=stderr;
+				}
+				
+				if(msg != null){
+					rl.setPrompt('');
+					rl.write('\n');				
+					console.log(msg);
+					rl.setPrompt(PROMPT);
+					rl.write('\n');
+				}
+			});	
 }
+
+function execSync(command) {
+	// Run the command in a subshell
+	child_process.exec(command + ' 2>&1 1>output && echo done! > done');
+	 
+	// Block the event loop until the command has executed.
+	while (!fs.existsSync('done')) {
+	// Do nothing
+	}
+	 
+	// Read the output
+	var output = fs.readFileSync('output',UTF8);
+	 
+	// Delete temporary files.
+	fs.unlinkSync('output');
+	fs.unlinkSync('done');
+	 
+	return output;
+}
+
+
+
+
+
